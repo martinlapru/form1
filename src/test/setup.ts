71eab2285fa0657f399cf5903427f2ebb75ac06b
martinlapru <martinprueba@oestecentro.com.ar>
2024-12-08 02:16:28 -0300

    Initial commit

---
+++ src/test/setup.ts
@@ -0,0 +1,12 @@
+import '@testing-library/jest-dom';
+import { expect, afterEach } from 'vitest';
+import { cleanup } from '@testing-library/react';
+import matchers from '@testing-library/jest-dom/matchers';
+
+// Extiende los matchers de Vitest
+expect.extend(matchers);
+
+// Limpia después de cada test
+afterEach(() => {
+  cleanup();
+});
